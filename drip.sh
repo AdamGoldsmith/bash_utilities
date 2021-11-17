54ced639060c279e632a11cb7798336b2a996230
Adam Admin <admin@example.com>
2021-11-17 12:38:37 +0000

    Add drip animation function

---
+++ drip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Drip animation
+# Adam Goldsmith
+
+drip() {
+  # USAGE: drip [drips (default: 1)] [speed (default: 0.2)] [keep drip (default 0)]
+  # drips is number of drip animations
+  # speed is in seconds
+  # keep drip: if non-zero keeps last drip character on-screen by moving cursor to the right
+  # Example: To have five drips at 1 character every 0.1 seconds and keep the drip on screen
+  # drip 5 0.1 1
+  local chars=("'" "!" ":" "." "," "_")
+
+  tput civis
+  for _ in $(seq 1 "${1:-1}")
+  do
+    for c in "${chars[@]}"
+    do
+      printf "%s" "${c}"
+      tput cub 1
+      sleep "${2:-0.2}"
+    done
+  done
+  tput cvvis
+  [[ "${3:-0}" -ne 0 ]] && tput cuf 1
+}
+
+drip 5 0.1 1
+
