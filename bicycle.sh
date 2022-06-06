94661f20eb580b4b6b64d6d3304a30c782c349c8
Adam Goldsmith <adam.goldsmith75@gmail.com>
2022-06-06 21:11:34 +0100

    Add acceleration parameter

--- bicycle.sh
+++ bicycle.sh
@@ -4,10 +4,11 @@
 # Adam Goldsmith
 
 ride() {
-  # USAGE: ride [display width (default: 8)] [speed (default: 0.1)]
+  # USAGE: ride [display width (default: 8)] [speed (default: 0.1)] [acceleration (default: 0%)]
   # speed is in seconds
-  # Example: To ride the bike in a 20 character window at 1 character every 0.05 seconds...
-  # ride 20 0.05
+  # accelerates to a maximum speed of 5 frames per second (1 per 0.2 secs)
+  # Example: To ride the bike in a 80 character window at 1 character every 0.4 seconds with 5% acceleration...
+  # ride 80 0.4 5
   local bdata=(
     "   __o"
     " _ \<_"
@@ -18,6 +19,9 @@ ride() {
     '\'
     '|'
   )
+  local speed="${2:-0.1}"
+  local acc
+  acc=$(bc <<< "scale=2; 1-${3:-0}/100")
 
   tput sc
   tput civis
@@ -29,10 +33,12 @@ ride() {
       printf "%*s%s\n" "${i}" "" "${l}"
     done
     printf "%*s(_)%s(_)\n" "${i}" "" "${pdata[$(( i % 4 ))]}"
-    sleep "${2:-0.1}"
+    sleep "${speed}"
+    # Have to divide by 1 to reset the scale - see bc documentation
+    [[ $(bc <<< "${speed}*100/1") -gt 1 ]] && speed=$(bc <<< "scale=4; ${speed}*${acc}")
   done
   tput cvvis
 }
 
-ride 20 0.05
+ride 80 0.4 5
 
