1f7900716eb39013b865fb9ed6d83d3e6ae7e568
Adam Goldsmith <adam.goldsmith75@gmail.com>
2022-06-07 12:21:11 +0100

    Bike whizzes off screen

--- bicycle.sh
+++ bicycle.sh
@@ -4,30 +4,29 @@
 # Adam Goldsmith
 
 ride() {
-  # USAGE: ride [display width (default: 8)] [speed (default: 0.1)] [acceleration (default: 0%)]
+  # USAGE: ride [display width (default: 20)] [speed (default: 0.1)] [acceleration (default: 0%)]
   # speed is in seconds
   # accelerates to a maximum speed of 5 frames per second (1 per 0.2 secs)
   # Example: To ride the bike in a 80 character window at 1 character every 0.4 seconds with 5% acceleration...
   # ride 80 0.4 5
+  local width="${1:-20}"
   local bdata=('   __o' ' _ \<_')
-  # shellcheck disable=SC1003
-  local pdata=('/' '-' '\' '|')
+  local pdata=('(_)/(_)' '(_)-(_)' '(_)\(_)' '(_)|(_)')
   local speed="${2:-0.1}"
-  local acc
-  acc=$(bc <<< "scale=2; 1-${3:-0}/100")
+  local acc; acc=$(bc <<< "scale=2; 1-${3:-0}/100")
 
   printf "\n\n\n"
   tput cuu1; tput cuu1; tput cuu1
   tput sc
   tput civis
-  for i in $(seq 0 1 "${1:-8}")
+  for i in $(seq 0 1 "${width}")
   do
     tput rc
     for l in "${bdata[@]}"
     do
-      printf "%*s%s\n" "${i}" "" "${l}"
+      printf "%*s%s\n" "${i}" "" "${l:0:$(( width - i ))}"
     done
-    printf "%*s(_)%s(_)\n" "${i}" "" "${pdata[$(( i % 4 ))]}"
+    printf "%*s%s\n" "${i}" "" "${pdata[$(( i % 4 ))]:0:(( width - i ))}"
     sleep "${speed}"
     # Have to divide by 1 to reset the scale - see bc documentation
     [[ $(bc <<< "${speed}*100/1") -gt 1 ]] && speed=$(bc <<< "scale=4; ${speed}*${acc}")
